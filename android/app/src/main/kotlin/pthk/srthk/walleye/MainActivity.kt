3dfeb830211c6eae1a68528fe9bad407aea0c592
Srthk Pthk <techysrthk@gmail.com>
2020-12-03 20:19:06 +0530

    move to home screen finished
    Signed-off-by: Srthk Pthk <techysrthk@gmail.com>

--- android/app/src/main/kotlin/pthk/srthk/walleye/MainActivity.kt
+++ android/app/src/main/kotlin/pthk/srthk/walleye/MainActivity.kt
@@ -4,6 +4,8 @@ import io.flutter.embedding.android.FlutterActivity
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import android.content.Intent
+
 
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
@@ -11,7 +13,11 @@ class MainActivity : FlutterActivity() {
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "android_app_retain").apply {
             setMethodCallHandler { method, rslt ->
                 if (method.method == "sendToHome") {
-                    moveTaskToBack(true)
+                    android.util.Log.d("v", "configureFlutterEngine: called")
+                    val homeIntent = Intent(Intent.ACTION_MAIN, null)
+                    homeIntent.addCategory(Intent.CATEGORY_HOME)
+                    homeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED)
+                    startActivity(homeIntent)
                 }
             }
         }
